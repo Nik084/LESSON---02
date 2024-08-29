7572b20be9a6ddd666fb013ef813e035979b9ff8
Nik084 <regul1200@gmail.com>
2024-08-29 22:11:09 +0300

    Add files via upload

---
+++ Modul 2 - 2.py
@@ -0,0 +1,9 @@
+first = int(input('введите первое целое число: '))
+second = int(input('введите второе целое число: '))
+third = int(input('введите третье целое число: '))
+if first == second == third:
+    print(3)
+elif first != second != third != first:
+    print(0)
+else:
+    print(2)
